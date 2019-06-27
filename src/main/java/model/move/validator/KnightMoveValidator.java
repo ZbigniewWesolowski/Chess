fbd550b55fc2e2886890be84d0cde737da0058bb
Zbnigniew Wesołowski <zbigniew.wesolowski123@gmail.com>
2019-06-27 09:05:21 +0200

    knightMoveValidator issues

---
+++ src/main/java/model/move/validator/KnightMoveValidator.java
@@ -0,0 +1,17 @@
+package model.move.validator;
+
+import model.board.ChessBoard;
+import model.move.Move;
+
+/**
+ * Created by 100-15 on 27.06.2019.
+ */
+public class KnightMoveValidator implements MoveValidatorFactory {
+    public boolean isValid (Move move, ChessBoard chessBoard) {
+        if (!((Math.abs(move.horizontalShift()) == 2 ) && (Math.abs(move.verticalShift())) == 1))
+        || ((Math.abs(move.horizontalShift()) == 1) && (Math.abs((move.verticalShift())) == 2)))) {
+            return false;
+        }
+        return true;
+    }
+}
