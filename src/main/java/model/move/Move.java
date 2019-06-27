889089e962d556f01fc6745148c060e5b2ce1af7
Zbnigniew Wesołowski <zbigniew.wesolowski123@gmail.com>
2019-06-27 08:31:43 +0200

    commit

--- src/main/java/model/move/Move.java
+++ src/main/java/model/move/Move.java
@@ -7,7 +7,7 @@ public class Move {
     private Position start;
     private Position end;
 
-    public Move (String from, String to) {
+    public Move(String from, String to) {
         start = Position.valueOf(from);
         end = Position.valueOf(to);
     }
@@ -28,5 +28,30 @@ public class Move {
         this.end = end;
     }
 
+    public int verticalShift() {
+        return end.getY() - start.getY();
+    }
 
+    public int horizontalShift() {
+        return end.getX() - start.getX();
+    }
+
+    public int diagonalShift() {
+        int xDelta = Math.abs(end.getX() - start.getX());
+        int yDelta = Math.abs(end.getY() - start.getY());
+        return xDelta == yDelta ? xDelta : 0;
+    }
+
+    public boolean isHorizontal () {
+        int absHorizontalShift = Math.abs(horizontalShift());
+        int absVerticalShift = Math.abs(verticalShift());
+        return absHorizontalShift > 0 && absVerticalShift == 0;
+    }
+
+    public boolean isVertical () {
+        int absHorizontalShift = Math.abs(horizontalShift());
+        int absVerticalShift = Math.abs(verticalShift());
+        return absHorizontalShift == 0 && absVerticalShift > 0;
+    }
 }
+
